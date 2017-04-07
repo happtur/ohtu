3c011e1d641979b612ad08b03616107d24b0de9f
happtur <andreaer@helsinki.fi>
2017-04-07 16:24:49 +0300

    teht. 4.1

---
+++ viikko4/TyhjaProjekti/src/main/java/ohtu/Main.java
@@ -0,0 +1,38 @@
+package ohtu;
+
+import com.google.gson.Gson;
+import java.io.IOException;
+import org.apache.http.client.fluent.Request;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        // vaihda oma opiskelijanumerosi seuraavaan, ÄLÄ kuitenkaan laita githubiin omaa opiskelijanumeroasi
+        String studentNr = "012345678";
+        if ( args.length>0) {
+            studentNr = args[0];
+        }
+
+        String url = "http://ohtustats2017.herokuapp.com/students/"+studentNr+"/submissions";
+
+        String bodyText = Request.Get(url).execute().returnContent().asString();
+
+//        System.out.println("json-muotoinen data:");
+//        System.out.println( bodyText );
+
+        Gson mapper = new Gson();
+        Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
+        
+        int totalTeht = 0;
+        int totalTunt = 0;
+        
+        System.out.println("opiskelijanumero: " + studentNr + "\n");
+        for (Submission submission : subs) {
+            System.out.println(submission);
+            totalTeht += submission.getTotal();
+            totalTunt += submission.getHours();
+        }
+        System.out.println("\nyhteensä: " + totalTeht + " tehtävää " + totalTunt + " tuntia\n");
+
+    }
+}
